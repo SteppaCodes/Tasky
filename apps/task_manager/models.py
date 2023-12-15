8c622144e0d33b8c1ee49889531852fb5f7da8bf
Big Steppa <steppacodes@gmail.com>
2023-12-15 21:27:27 +0100

    feaure:Flow. Setp flow model and order field

--- apps/task_manager/models.py
+++ apps/task_manager/models.py
@@ -3,6 +3,7 @@ from django.db import models
 #local imports
 from apps.common.models import BaseModel
 from apps.accounts.models import User
+from .fields import OrderField
 
 TASK_STATUS = (("not started", "not Started"),("completed", "completed"))
 
@@ -14,3 +15,18 @@ class Task(BaseModel):
     def __str__(self):
         return self.name
 
+class Flow(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="flows")
+    name = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+    
+
+class FlowTask(BaseModel):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    flow = models.ForeignKey(Flow, on_delete=models.CASCADE)
+    order = OrderField(blank=True, for_field=["flow"])
+
+    def __str__(self):
+        return self.task.name
