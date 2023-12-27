419e0e1f4d68a257b6bd808ffc3e5cf0982c8a9b
Big Steppa <steppacodes>
2023-12-27 17:15:17 +0100

    Field: Parenttask that sets parent task automatically

--- apps/task_manager/models.py
+++ apps/task_manager/models.py
@@ -1,9 +1,10 @@
+from collections.abc import Iterable
 from django.db import models
 
 #local imports
 from apps.common.models import BaseModel
 from apps.accounts.models import User
-from .fields import OrderField
+from .fields import OrderField, ParentTaskField
 
 TASK_STATUS = (("not started", "not Started"),("completed", "completed"))
 
@@ -21,12 +22,13 @@ class Flow(BaseModel):
 
     def __str__(self):
         return self.name
-    
+   
 
 class FlowTask(BaseModel):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    flow = models.ForeignKey(Flow, on_delete=models.CASCADE)
+    flow = models.ForeignKey(Flow, on_delete=models.CASCADE, related_name="flowtasks")
     order = OrderField(blank=True, for_field=["flow"])
+    parent_task = ParentTaskField(blank=True, null=True, for_field=["flow"])
 
     def __str__(self):
         return self.task.name
